6a6d2ef9fbea8995637fddba399b0c49919b2b1a
Dawei <wangdwes@gmail.com>
2015-02-01 16:24:01 -0500

    mwptraj bug fixed (cumtau)

--- project1b/mwptraj.m
+++ project1b/mwptraj.m
@@ -1,8 +1,9 @@
-function [trajs, t] = mwptraj(coeff, tau),
+function [trajs, t] = mwptraj(coeff, tau)
 
-t = []; trajs = cell(size(coeff)); 
+t = []; trajs = cell(size(coeff)); cumtau = 0; 
 % yeah so i put everything on a single line - bite me! despite the fact that two loops are used, 
 % the actual computational burden is not expected to be huge - only ten or twenty iterations needed. 
-for idx = 1: numel(tau) - 1, tl = linspace(0, tau(idx + 1)); t = horzcat(t, tl + tau(idx));
+for idx = 1: numel(tau) - 1, 
+  tl = linspace(0, tau(idx + 1)); cumtau = cumtau + tau(idx); t = horzcat(t, tl + cumtau);  
   for vi = 1: length(coeff), trajs{vi} = horzcat(trajs{vi}, polyval(coeff{vi}(:, idx), tl)); end, end
 

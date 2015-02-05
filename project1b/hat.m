94068d4d98406acd51ea901c3ce687f7c9d793c2
Dawei <wangdwes@gmail.com>
2015-02-05 01:36:11 -0500

    removed imu. performance ok.

--- project1b/hat.m
+++ project1b/hat.m
@@ -1,2 +1,2 @@
-function mat = hat(vec), mat = @(vec) [0, -vec(3), vec(2); vec(3), 0, -vec(1); -vec(2), vec(1), 0];
+function mat = hat(vec), mat = [0, -vec(3), vec(2); vec(3), 0, -vec(1); -vec(2), vec(1), 0];
 

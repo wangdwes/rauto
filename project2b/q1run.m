b9d72de53196a0fadf89114c3d285902c0f3ab02
Dawei <wangdwes@gmail.com>
2015-03-06 13:11:43 -0500

    before - norm for each iter.

--- project2b/q1run.m
+++ project2b/q1run.m
@@ -27,14 +27,22 @@ for index = 2: length(left_image_names)
   locs1r = bsxfun(@rdivide, locs1(:, 1: 3), locs1(:, 3));
   locs2r = bsxfun(@rdivide, locs2(:, 1: 3), locs2(:, 3));
 
-%  showMatchedFeatures(im1, im2, locs1r(:, 1: 2), locs2r(:, 1: 2));
-%  title(num2str(index));
-%  waitforbuttonpress;
+%    locs1r = [locs1, ones(size(locs1, 1), 1)];
+%    locs2r = [locs2, ones(size(locs2, 1), 1)];
+
+%  if mod(index, 2) == 0, 
+%    showMatchedFeatures(im1, im2, locs1r(:, 1: 2), locs2r(:, 1: 2));
+%  else
+%    showMatchedFeatures(im1, im2, locs1r(:, 1: 2), locs2r(:, 1: 2));
+%  end
+
+%  title(num2str(index)); 
+%  waitforbuttonpress; clf
 
   [rot, tr] = camtrf(locs1r, locs2r, fm, intriparams);
-  points(:, index) = point; point = rot * (point + tr); index
+  points(:, index) = point; point = rot * (point - tr); index
 
-  rrot = rrot * rot; 
+  rrot = rrot * rot; det(rrot) 
 
 %  thetas(:, index) = rotationMatrix2eulerAngles(rrot);
 

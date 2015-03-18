af536c477910f1d7bc1186ef91e90cbf14cfe053
Dawei <wangdwes@gmail.com>
2015-03-18 00:15:48 -0400

    using lm - starting to look like sth.

--- project2b/cameramat.m
+++ project2b/cameramat.m
@@ -1,9 +1,13 @@
 
 function cm = cameramat(locs3d, locs2d, method) 
 
+  % normalize the points as suggested in hartley, zisserman.  
+  [locs3d, transmat3d] = normalize(locs3d);
+  [locs2d, transmat2d] = normalize(locs2d); 
+
   % write something random to make this look well documented.  
   count = size(locs3d, 1); inliers = false(count, 1);
-  cm = eye(3, 4); niter = 0; maxiter = 200; tol = 1;   
+  cm = eye(3, 4); niter = 0; maxiter = 200; tol = 1e-3;   
 
   % iterate until the maximum number of iterations is hit. 
   while niter < maxiter, 
@@ -13,13 +17,9 @@ function cm = cameramat(locs3d, locs2d, method)
     selected = randperm(count, 6); 
     cm = dlt6p(locs3d(selected, :), locs2d(selected, :));
 
-    % compute the reprojection error. 
-    rplocs = locs3d * cm'; rplocs = bsxfun(@rdivide, rplocs, rplocs(:, 3));
-    dists = sum((rplocs(:, 1: 2) - locs2d(:, 1: 2)) .^ 2, 2); 
-
-    niter = niter + 1;
+    dists = sum(reprojerr(locs3d, locs2d, cm) .^ 2, 2); niter = niter + 1;
     % if there're more inliers than previously stored, take it.
-    if sum(inliers) < sum(dists < tol ^ 2),
+    if sum(inliers) < sum(dists < tol),
       inliers = dists < tol;
       % implemented here is a sub-routine that updates the number of iterations
       % based on our desired confidence level and ratio of inliers.
@@ -31,15 +31,24 @@ function cm = cameramat(locs3d, locs2d, method)
     end
   end
 
-  cm = dlt6p(locs3d(inliers, :), locs2d(inliers, :)); 
-  % the model may be improved by re-estimating it
-  % using all memeber of the consensus set. 
+  locs3d = locs3d(inliers, :); locs2d = locs2d(inliers, :); cm = dlt6p(locs3d, locs2d); 
+  % i do apologize for having to write the same thing twice.
+  % here we're invoking the levenberg-marquardt routine to optimize the solution.  
+  options = optimoptions('lsqnonlin', 'Algorithm', 'levenberg-marquardt', 'Display', 'none'); 
+  cm = lsqnonlin(@(vec) reprojerr(locs3d, locs2d, reshape(vec, 4, 3)'), reshape(cm', [], 1), [], [], options); 
+  cm = inv(transmat2d) * reshape(cm, 4, 3)' * transmat3d; 
 
   function cm = dlt6p(locs3d, locs2d)
     % each 3d-2d point correspondence contributes two independent equations. 
     coeff = [locs3d, zeros(size(locs3d)), -bsxfun(@times, locs2d(:, 1), locs3d); ...
              zeros(size(locs3d)), locs3d, -bsxfun(@times, locs2d(:, 2), locs3d)]; 
     % obtain the eigenvector corresponding to the minimum eigenvalue. 
+    % important: normalize the matrix or our dists computation will be meaningless.  
     [~, ~, eigenvecs] = svd(coeff); cm = reshape(eigenvecs(:, end), 4, 3)'; end
+
+  function err = reprojerr(locs3d, locs2d, cm)
+    % computing the reprojection error 
+    rplocs = locs3d * cm'; rplocs = bsxfun(@rdivide, rplocs, rplocs(:, 3)); 
+    err = rplocs(:, 1: 2) - locs2d(:, 1: 2); end
 
 end
--- project2b/q1run.m
+++ project2b/q1run.m
@@ -4,45 +4,30 @@ load(fullfile('sensor', 'hand_carry.mat'));
 load(fullfile('assets', 'useful.mat')); translation = [-0.1621, 0, 0]';
 
 [point] = zeros(3, 1); count = length(left_image_names);
-[points, thetas] = deal(zeros(3, count)); cumrot = eye(3);
+[points] = deal(zeros(3, count)); h = waitbar(0, 'Please wait...'); 
 
 % iterate through all successive images and find their incremental transformation. 
-for index = 1: count - 1, index, 
+for index = 1: count - 1, waitbar(index / count, h); 
 
   % extract features and find correspondences for stereo image pairs.
   % and invoke our triangulate method to find our their three-dimensional points.  
   [iml, featl, vptl] = impreproc(left_image_names{index});
   [imr, featr, vptr] = impreproc(right_image_names{index}); 
   [matches] = matchFeatures(featl, featr); 
-  [locs3d] = triangulateb(vptl(matches(:, 1), :), vptr(matches(:, 2), :), ...
-                         intriparams * eye(3, 4), intriparams * horzcat(eye(3), translation), '');  
-
-  [locs3d] = triangulate(intriparams * eye(3, 4), ...
-                         vptl(matches(:, 1), 1: 2)', ...
-                         intriparams * horzcat(eye(3), translation), ...
-                         vptr(matches(:, 2), 1: 2)'); 
-
-  locs3d = horzcat(locs3d', ones(size(locs3d, 2), 1)); 
+  [locs3d] = triangulate(vptl(matches(:, 1), :), vptr(matches(:, 2), :), ...
+                         intriparams * eye(3, 4), intriparams * horzcat(eye(3), translation));
 
   % this is apparently not efficient - but for proof of concept it should be fine.  
   [ims, feats, vpts] = impreproc(left_image_names{index + 1}); 
   [matchest] = matchFeatures(featl, feats);
-  [~, ia, ib] = intersect(matches(:, 1), matchest(:, 1)); 
+  [~, ia, ib] = intersect(matches(:, 1), matchest(:, 1));
   [~, locb] = ismember(matches(ia, 1), matches(:, 1)); 
 
   % compute the camera matrix for later decomposition into r and t.  
   cm = cameramat(locs3d(locb, :), vpts(matchest(ib, 2), :)); 
-  extriparams = inv(intriparams) * cm; 
-  extriparams = extriparams / det(extriparams(:, 1: 3)) ^ (1 / 3);  
+  [rot, tr] = camat2rt(cm / cm(3, 3)); 
 
   % store some euler angles as well for easier plotting.
-  points(:, index) = point; 
-  point = extriparams(:, 1: 3) * (point - extriparams(:, 4)); 
-
-  % thetas(1, index) = atan2(cumrot(3, 2), cumrot(3, 3));
-  % thetas(2, index) = atan2(-cumrot(3, 1), sqrt(cumrot(3, 2).^2+cumrot(3,3)^2));
-  % thetas(3, index) = atan2(cumrot(2, 1), cumrot(1, 1));
+  points(:, index) = point; point = rot * point - tr;
 
 end
-
-   
